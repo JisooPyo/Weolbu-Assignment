4e15e112d23a11ea1d7bf06a236419e26057f44b
JisooPyo <audwl326@gmail.com>
2024-06-11 00:30:20 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/test/java/weolbu/assignment/WeolbuProjectPyojisooApplicationTests.java
@@ -0,0 +1,13 @@
+package weolbu.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeolbuProjectPyojisooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
