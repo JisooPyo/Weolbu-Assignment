f3355e3415046d24c2f9fc2022d7adac6d9775a6
JisooPyo <audwl326@gmail.com>
2024-06-14 12:28:28 +0900

    update: CoursesResponseDto 생성자 추가

--- src/main/java/weolbu/assignment/course/dto/CoursesResponseDto.java
+++ src/main/java/weolbu/assignment/course/dto/CoursesResponseDto.java
@@ -2,11 +2,15 @@ package weolbu.assignment.course.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CoursesResponseDto {
     long totalElements;
     int totalPages;
