eafd6ec1112b23982fb6348372930a47355cb568
JisooPyo <audwl326@gmail.com>
2024-06-14 17:57:27 +0900

    feat: Swagger 적용

--- src/main/java/weolbu/assignment/course/controller/CourseController.java
+++ src/main/java/weolbu/assignment/course/controller/CourseController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import weolbu.assignment.common.dto.ApiResponseDto;
@@ -20,22 +22,26 @@ import weolbu.assignment.course.service.CourseService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/courses")
+@Tag(name = "Course API", description = "강의 관련 API 정보를 담고 있습니다.")
 public class CourseController {
     private final CourseService courseService;
 
     @PostMapping
+    @Operation(summary = "강의 개설 API")
     public ResponseEntity<ApiResponseDto> createCourse(@Valid @RequestBody CreateCourseRequestDto requestDto) {
         ApiResponseDto response = courseService.createCourse(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PostMapping("/application")
+    @Operation(summary = "강의 신청 API")
     public ResponseEntity<ApiResponseDto> applyCourse(@Valid @RequestBody ApplyCourseRequestDto requestDto) {
         ApiResponseDto response = courseService.applyCourse(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
+    @Operation(summary = "강의 목록 조회 API")
     public ResponseEntity<CoursesResponseDto> getCourses(
         @RequestParam(defaultValue = "recent") String sortBy,
         @RequestParam(defaultValue = "1") int page,
--- src/main/java/weolbu/assignment/member/controller/MemberController.java
+++ src/main/java/weolbu/assignment/member/controller/MemberController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import weolbu.assignment.common.dto.ApiResponseDto;
@@ -16,11 +18,13 @@ import weolbu.assignment.member.service.MemberService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
+@Tag(name = "Member API", description = "Member 관련 API 정보를 담고 있습니다.")
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입 API")
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto){
+    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) {
         ApiResponseDto response = memberService.signup(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
